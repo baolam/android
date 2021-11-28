dd207d1e52445a8e71c1e2f722614e76e2df1272
baolam <lam01662052827@gmail.com>
2021-11-28 15:23:22 +0700

    Gui xe thong minh bang dung qr code cua google vision

---
+++ app/src/main/java/com/example/chinh_thuc/CarformActivity.java
@@ -0,0 +1,14 @@
+package com.example.chinh_thuc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarformActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carform);
+    }
+}
